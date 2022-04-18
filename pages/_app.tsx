ab1c30f443736c7ce50327375f1ab40b2c2d339b
doyoung <ldy9212@gmail.com>
2022-04-18 20:50:27 +0900

    [2022-04] template init

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import '/styles/global.css';
+import { theme } from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
