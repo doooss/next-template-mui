d2b5736d4cc93c314caf88ab7649cf29d253a34b
doyoung <ldy9212@gmail.com>
2022-04-19 23:31:07 +0900

    [2022-04] react 18 hydrate bug fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 import '/styles/global.css';
 import { theme } from '../styles/theme';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) return null;
+
+  if (window === undefined) return <></>;
+
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
